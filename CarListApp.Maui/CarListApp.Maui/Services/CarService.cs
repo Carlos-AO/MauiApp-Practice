784c26eb63545105cee4fcdc8e04c5fa1b4d11bc
Carlos-AO <93808290+Carlos-AO@users.noreply.github.com>
2023-12-19 10:04:39 +1000

    initiate project

---
+++ CarListApp.Maui/CarListApp.Maui/Services/CarService.cs
@@ -0,0 +1,35 @@
+﻿using CarListApp.Maui.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListApp.Maui.Services
+{
+    public class CarService
+    {
+        public List<Car> GetCars()
+        {
+            return new List<Car>() 
+            {
+                new Car 
+                { 
+                    Id = 1, Make = "Honda", Model = "Fit", Vin = "123"
+                },
+                new Car 
+                {
+                    Id  = 2, Make = "Toyota", Model = "Prado", Vin = "234"
+                },
+                new Car
+                {
+                    Id  = 3, Make = "Audi", Model = "Civic", Vin = "523"
+                },
+                new Car
+                {
+                    Id = 4, Make = "BMW", Model = "M3", Vin = "623"
+                },
+            };
+        }
+    }
+}
