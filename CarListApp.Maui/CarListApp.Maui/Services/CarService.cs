1f83d04fd35a78a9ddb9c1635bedbb156f9de61d
Carlos-AO <93808290+Carlos-AO@users.noreply.github.com>
2023-12-21 08:22:52 +1000

    List sample added

--- CarListApp.Maui/CarListApp.Maui/Services/CarService.cs
+++ CarListApp.Maui/CarListApp.Maui/Services/CarService.cs
@@ -11,13 +11,13 @@ namespace CarListApp.Maui.Services
     {
         public List<Car> GetCars()
         {
-            return new List<Car>() 
+            return new List<Car>()
             {
-                new Car 
-                { 
+                new Car
+                {
                     Id = 1, Make = "Honda", Model = "Fit", Vin = "123"
                 },
-                new Car 
+                new Car
                 {
                     Id  = 2, Make = "Toyota", Model = "Prado", Vin = "234"
                 },
@@ -27,8 +27,36 @@ namespace CarListApp.Maui.Services
                 },
                 new Car
                 {
-                    Id = 4, Make = "BMW", Model = "M3", Vin = "623"
+                    Id = 4, Make = "BMW", Model = "M4", Vin = "623"
                 },
+                new Car
+                {
+                    Id = 4, Make = "1BMW", Model = "M5", Vin = "1623"
+                },
+                new Car
+                {
+                    Id = 4, Make = "2BMW", Model = "M6", Vin = "2623"
+                },
+                new Car
+                {
+                    Id = 4, Make = "3BMW", Model = "M7", Vin = "3623"
+                },
+                new Car
+                {
+                    Id = 4, Make = "4BMW", Model = "M83", Vin = "4423"
+                },
+                new Car
+                {
+                    Id = 1, Make = "1Honda", Model = "Fit", Vin = "3123"
+                },
+                new Car
+                {
+                    Id  = 2, Make = "2Toyota", Model = "Prado", Vin = "3234"
+                },
+                new Car
+                {
+                    Id  = 3, Make = "3Audi", Model = "Civic", Vin = "3523"
+                }
             };
         }
     }
